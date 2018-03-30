3c4ea77cd5f7ba6b732c03a936c9bf5ba2176238
kry-dev <yaroslavkryvda@gmail.com>
2018-03-30 15:00:00 +0300

    created first craft page

--- src/js/common/scrollTop.js
+++
@@ -1,9 +0,0 @@
-// export scrollFunction = function() {
-//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//         document.getElementById("scrollTop").style.display = "block";
-//     } else {
-//         document.getElementById("scrollTop").style.display = "none";
-//     }
-// };
-
-
