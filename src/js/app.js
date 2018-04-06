5902e4dfe54241f2039d1a2df4a985d8594378d5
kry-dev <yaroslavkryvda@gmail.com>
2018-04-07 00:13:12 +0300

    add modal form with map/ adress autocomplete

--- src/js/app.js
+++ src/js/app.js
@@ -190,7 +190,8 @@ $( document ).ready(function(){
                     '<div class="map-tooltip-content">'+
                     '<div class="map-tooltip-content-left">' +
                     '<p>71, rue de Tolbiac,<br /> PARIS 75013,<br /> Magasin vrac </p>'+
-                    '<a href="#" class="btn btn-warning">'+ 'More information</a>'+
+                    '<button class="btn btn-warning" type="button" data-toggle="modal" data-target="#mapMarkerModal">'+ 'More' +
+                    ' information</button>'+
                     '</div>'+
                     '<div class="map-tooltip-content-right"><img src="assets/img/map/map-pin-01.png"></div>'+
                     '</div>'+
@@ -239,5 +240,60 @@ $( document ).ready(function(){
             hide(cat);
         }
     });
+
+    (function initMap() {
+        var map = new google.maps.Map(document.getElementById('map_modal'), {
+            center: {lat: -33.8688, lng: 151.2195},
+            zoom: 13
+        });
+        var input = /** @type {!HTMLInputElement} */(
+            document.getElementById('pac-input'));
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+            map: map,
+            anchorPoint: new google.maps.Point(0, -29)
+        });
+        autocomplete.addListener('place_changed', function() {
+            infowindow.close();
+            marker.setVisible(false);
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                // User entered the name of a Place that was not suggested and
+                // pressed the Enter key, or the Place Details request failed.
+                window.alert("No details available for input: '" + place.name + "'");
+                return;
+            }
+
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);  // Why 17? Because it looks good.
+            }
+            marker.setIcon(/** @type {google.maps.Icon} */({
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(35, 35)
+            }));
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+            var address = '';
+            if (place.address_components) {
+                address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || '')
+                ].join(' ');
+            }
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+            infowindow.open(map, marker);
+        });
+    })();
 });
-//
