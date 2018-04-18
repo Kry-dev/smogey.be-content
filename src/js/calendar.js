1452b46e7adccccc0f667eb618b9bc7f75db9ce5
kry-dev <yaroslavkryvda@gmail.com>
2018-04-19 01:00:54 +0300

    fix map

--- src/js/calendar.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.events-item').click(function (e) {
-        e.preventDefault();
-        console.log('click');
-        var tooltip = $(this).find('.calendar-tooltip');
-        $(tooltip).toggle();
-    })
-};
